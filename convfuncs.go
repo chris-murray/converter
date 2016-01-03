602af476ef7f2f13128335f2bf771d17e14d4b83
Chris Murray <chris@ipstuff.ca>
2016-01-02 21:28:37 -0800

    fixes

--- convfuncs.go
+++ convfuncs.go
@@ -2,12 +2,12 @@
 package converter
 
 // return the number of parsecs for the given distance in lightyears
-func LRtoParsec (lr LightYear) Parsec {
+func LYtoParsec (lr LightYear) Parsec {
   return Parsec(lr * ParsecRef )
 }
 
 // return the number of parsecs for the given distance in lightyears
-func PtoLR (p Parsec) LightYear {
+func PtoLY (p Parsec) LightYear {
   return LightYear(p * LyInP )
 }
 
