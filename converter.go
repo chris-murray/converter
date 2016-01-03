e165880d87b9301f6e6d5ccb73cc10dbccd76fbe
Chris Murray <chris@ipstuff.ca>
2016-01-02 21:44:13 -0800

    run go fmt sooner

--- converter.go
+++ converter.go
@@ -7,9 +7,9 @@ type LightYear float64
 type Parsec float64
 
 const (
-	PinLY Parsec = 0.306601
-  LYInP LightYear = 3.26
+	PinLY Parsec    = 0.306601
+	LYInP LightYear = 3.26
 )
 
-func (p Parsec) String() string    { return fmt.Sprintf("%gp", p) }
+func (p Parsec) String() string     { return fmt.Sprintf("%gp", p) }
 func (ly LightYear) String() string { return fmt.Sprintf("%gly", ly) }
--- convfuncs.go
+++ convfuncs.go
@@ -2,14 +2,13 @@
 package converter
 
 // return the number of parsecs for the given distance in lightyears
-func LYtoParsec (ly LightYear) Parsec {
-  return Parsec(ly) * PinLY
+func LYtoParsec(ly LightYear) Parsec {
+	return Parsec(ly) * PinLY
 }
 
 // return the number of parsecs for the given distance in lightyears
-func PtoLY (p Parsec) LightYear {
-  return LightYear(p) * LYInP
+func PtoLY(p Parsec) LightYear {
+	return LightYear(p) * LYInP
 }
-
 
 //!-
